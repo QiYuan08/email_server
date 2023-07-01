1981a6ee2f8dababca31fdd2caf7492180bb601f
QiYuan08 <qteh0001@student.monash.edu>
2023-07-01 09:44:23 +0800

    changed to magit email

--- imap_new.js
+++ imap_new.js
@@ -22,108 +22,101 @@ const readMail = async (mailID) => {
   // and simpleParser is able to process the stream as a Promise
   const parsed = await simpleParser(content);
 
-  // get the ticketID
-  superagent
-    .post(
-      "https://us-central1-ticketing-60a94.cloudfunctions.net/mailer/get-id"
-    )
-    .set("Content-Type", "application/json")
-    .send({
-      references: parsed.references ?? ["none"],
-    })
-    .set("Accept", "application/json")
-    .then(async (response) => {
-      let data = response.body;
-      console.log(data);
-      let ticketID = data.exist
-        ? data.ticketID
-        : "TK" + Math.floor(100000 + Math.random() * 900000);
+  let apiCall = superagent.post("http://127.0.0.1:8000/api/createTicket");
+  // .set("Content-Type", "multipart/form-data")
 
-      let attachmentArr = [];
-      let firebaseMessageId = v4();
+  console.log("haahahhaha", parsed.attachments.length);
 
-      await Promise.all(
-        parsed.attachments.map(async (item) => {
-          attachmentArr.push({
-            filename: item.filename,
-            size: item.size,
-            // contentBuffer: item.content,
-          });
+  parsed.attachments.map(async (attachment) => {
+    // let filePath = await writeToFile(attachment.filename, attachment.content);
 
-          await writeToFile(
-            item.filename,
-            item.content,
-            ticketID,
-            firebaseMessageId
-          );
-        })
-      );
+    // console.log(1, filePath);
+    apiCall.attach(
+      attachment.filename,
+      attachment.content,
+      attachment.filename
+    );
+  });
 
-      // https://us-central1-ticketing-60a94.cloudfunctions.net/mailer/create-ticket
-      // "http://127.0.0.1:5001/ticketing-60a94/us-central1/mailer/create-ticket"
+  console.log(" reply to: " + parsed.inReplyTo ?? "");
+  console.log(" references to: " + parsed.references);
 
-      superagent
-        .post(
-          "https://us-central1-ticketing-60a94.cloudfunctions.net/mailer/create-ticket"
-        )
-        .set("Content-Type", "application/json")
-        .send({
-          ticketID: ticketID,
-          firebaseMessageId: firebaseMessageId,
-          references: parsed.references,
-          email: parsed.from.value[0].address,
-          sendMail: false,
-          messageId: parsed.messageId,
-          subject: parsed.headers?.get("subject") ?? "",
-          message: parsed.html ?? parsed.textAsHtml ?? parsed.text,
-          to: parsed.to.value[0].address,
-          attachments: attachmentArr,
-          cc: parsed.cc?.value?.map((addr) => addr.address) ?? [],
-        })
-        .set("Accept", "*/*")
-        .then((response) => {
-          writeLog(response);
-        })
-        .catch((err) => {
-          writeLog(err);
-        });
-    })
-    .catch((error) => {
-      console.error(error);
+  apiCall
+    .field("inReplyTo", parsed.inReplyTo ?? "")
+    .field("from", parsed.from.value[0].address)
+    .field("messageId", parsed.messageId)
+    .field("fromName", parsed.from.value[0].name)
+    .field("subject", parsed.headers?.get("subject") ?? "")
+    .field("message", parsed.html ?? parsed.textAsHtml ?? parsed.text)
+    .field("to", parsed.to.value[0].address)
+    .field("cc", parsed.cc?.value?.map((addr) => addr.address) ?? [])
+    .end((err, response) => {
+      if (err) {
+        console.log(err);
+        writeLog(err);
+      } else {
+        writeLog(response);
+        console.log("1233432534523656536", response.body);
+      }
     });
+
+  // apiCall
+  //   .send({
+  // ticketID: ticketID,
+  // firebaseMessageId: firebaseMessageId,
+  // references: parsed.references,
+  // from: parsed.from.value[0].address,
+  // fromName: parsed.from.value[0].name,
+  // sendMail: false,
+  // messageId: parsed.messageId,
+  // subject: parsed.headers?.get("subject") ?? "",
+  // message: parsed.html ?? parsed.textAsHtml ?? parsed.text,
+  // to: parsed.to.value[0].address,
+  // attachments: parsed.attachments,
+  // cc: parsed.cc?.value?.map((addr) => addr.address) ?? [],
+  //   })
+  //   .set("Accept", "application/json")
+  // .then((response) => {
+  //   writeLog(response);
+  //   console.log("1233432534523656536", response);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  //   writeLog(err);
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  // });
 };
 
 const isObject = (value) => {
   return value != null && typeof value == "object" && !Array.isArray(value);
 };
 
-const writeToFile = async (fileName, data, ticketID, messageId) => {
+const writeToFile = async (fileName, data) => {
   let filehandle = null;
   const tmpPath = path.join(os.tmpdir(), fileName);
+  // path.join("uploads/", fileName);
 
-  try {
-    filehandle = await fs.promises.open(tmpPath, (mode = "w"));
-    // Write to file
-    await filehandle.writeFile(data, { encoding: "base64" });
-  } finally {
-    if (filehandle) {
-      // Close the file if it is opened.
-      await filehandle.close();
-      // console.log("finished reading");
-    }
-  }
+  fs.writeFileSync(tmpPath, data, { encoding: "base64", flag: "w" });
 
-  const destination = `${ticketID}/${messageId}/${fileName}`;
+  console.log(tmpPath);
+  return tmpPath;
 
-  try {
-    await bucket.upload(tmpPath, {
-      destination: destination,
-    });
-  } catch (err) {
-    console.error("Send mail error: ", err);
-  }
+  //   filehandle = await fs.promises.open(tmpPath, "w");
+  //   // Write to file
+  //   await filehandle.writeFile(data, { encoding: "base64" });
+  //   return tmpPath;
+  // } finally {
+  //   if (filehandle) {
+  //     // Close the file if it is opened.
+  //     console.log(2);
+  //     await filehandle.close();
+  //     // console.log("finished reading");
+  //   }
+  // }
 
-  writeLog("successfully uploaded to " + ticketID, messageId);
+  // writeLog("successfully uploaded to " + ticketID, messageId);
 };
 
 const writeLog = (log) => {
@@ -136,19 +129,19 @@ const writeLog = (log) => {
 
 const main = async () => {
   client = new ImapFlow({
-    host: "imap.gmail.com",
+    // host: "imap.gmail.com",
+    host: "cloudmail103.zonecybersite.com",
     port: 993,
     secure: true,
     auth: {
-      user: userEmail,
+      user: "support@magit.sg",
+      pass: "Mag@1234$$",
 
-      // magit.sg
+      // user: "chaelqi89@gmail.com",
       // pass: "ghpdcpxcsgviyczr",
 
-      pass: "bshhoioghiccfidd",
-
-      // chaelqi89gmail
-      // pass: "zvevmwhhwvoamzha",
+      // user: "magittester@gmail.com",
+      // pass: "bshhoioghiccfidd",
     },
   });
 
